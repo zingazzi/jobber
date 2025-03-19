23add874f3c6e0eeeeaca43f259b9c74db20237f
Marco Zingoni <marco.zingoni@gmail.com>
2025-03-19 07:22:39 +0100

    Add current user decorator

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { AuthModule } from './auth/auth.module';
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
       autoSchemaFile: true,
       context: ({ req, res }) => ({ req, res }),
     }),
